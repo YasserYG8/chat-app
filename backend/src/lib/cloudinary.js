8a2e2c921e15c67b6719d4aac2d056badad73399
yasser <yasserg4e@gmail.com>
2025-08-25 18:48:13 +0100

    update

--- backend/src/lib/cloudinary.js
+++ backend/src/lib/cloudinary.js
@@ -6,7 +6,7 @@ config();
 
 cloudinary.config({
     cloud_name  : 'dmyzblvoi',
-    api_key : '364198119611463',
-    api_secret : 'KAWc4GspmCdJ_R7rDWWVSdpzuMM'
+    api_key : process.env.api_key,
+    api_secret : process.env.api_secret
 });
 export default cloudinary;
